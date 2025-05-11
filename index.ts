9d2efbbfe3aa68e45ea1527da4cf2a9f8b52a212
artcrm2025 <info@artcrm.com.tr>
2025-05-11 23:53:04 +0300

    index.ts dosyası eklendi

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { registerRootComponent } from 'expo';
+import App from './App';
+
+// registerRootComponent, Expo'nun kullanımı için gereken kök bileşenini kaydeder
+// Bu, React Native'in AppRegistry.registerComponent() işlevine benzer
+registerRootComponent(App);
