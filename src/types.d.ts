2dd8e7f8ba6c200d861d84bf3b5069092351d413
artcrm2025 <info@artcrm.com.tr>
2025-05-12 00:10:54 +0300

    Modül tanımlarını içeren types.d.ts dosyasını ekliyorum

---
+++ src/types.d.ts
@@ -0,0 +1,38 @@
+declare module 'react-native-html-to-pdf' {
+  interface RNHTMLtoPDFOptions {
+    html: string;
+    fileName?: string;
+    directory?: string;
+    base64?: boolean;
+    height?: number;
+    width?: number;
+    padding?: number;
+  }
+
+  interface RNHTMLtoPDFResponse {
+    filePath: string;
+    base64?: string;
+  }
+
+  const RNHTMLtoPDF: {
+    convert: (options: RNHTMLtoPDFOptions) => Promise<RNHTMLtoPDFResponse>;
+  };
+
+  export default RNHTMLtoPDF;
+}
+
+declare module 'react-native-print' {
+  interface PrintOptions {
+    jobName?: string;
+    filePath?: string;
+    html?: string;
+    printerURL?: string;
+  }
+
+  const RNPrint: {
+    print: (options: PrintOptions) => Promise<void>;
+    selectPrinter: () => Promise<void>;
+  };
+
+  export default RNPrint;
+}
